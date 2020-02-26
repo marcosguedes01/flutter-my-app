5b673298d425a6d61b91b682363f18c2d5dcd8d0
Marcos Guedes <mguedes@growin.pt>
2020-02-26 23:09:48 +0000

    change the UI

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Startup Name Generator',
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),  
       home: RandomWords(),
     );
   }
@@ -54,7 +57,7 @@ class RandomWordsState extends State<RandomWords> {
               );
             },
           );
-          
+
           final List<Widget> divided = ListTile
             .divideTiles(
               context: context,
